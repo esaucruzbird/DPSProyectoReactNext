c1c58afbd1845809f337314ee4a1ed044bc4b6ba
esaucruzbird <esaucruzbird@gmail.com>
2025-09-18 00:22:10 -0600

    Se corrije la estructura deTailwind en los archivos postcss config cjs y en globals css Se deshabilita turbopack en package json para dev

---
+++ postcss.config.cjs
@@ -0,0 +1,31 @@
+/*const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+
+export default config;*/
+
+/*const config = {
+  plugins: ["tailwindcss", "autoprefixer"],
+};
+
+export default config;*/
+
+// postcss.config.mjs
+/*const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+};
+export default config;*/
+
+
+// postcss.config.cjs
+module.exports = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+};
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,16 @@
 // tailwind.config.js
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
 
 
